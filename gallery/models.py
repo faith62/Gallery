9b0e2beca0edfe9c60d6f77e93ae92b4474ddead
faith62 <phifeichemu@gmail.com>
2022-05-29 02:08:12 +0300

    unnitest

--- gallery/models.py
+++ gallery/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-
+    pub_date = models.DateTimeField(auto_now_add=True, blank=True, null = True)
 
     def __str__(self):
         return self.name
