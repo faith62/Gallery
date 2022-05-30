9b90e68442ebd406729c9f7328f1aeb0b8b76e4b
faith62 <phifeichemu@gmail.com>
2022-05-30 20:58:15 +0300

    update the search resu;ts view

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 
-from .models import Image
+from .models import Category, Image, Location
 
 # Create your views here.
 def welcome(request):
@@ -13,12 +13,14 @@ def welcome(request):
 
 def search_results(request):
 
+    categories = Category.objects.all()
+    locations = Location.objects.all()
     if 'image' in request.GET and request.GET["image"]:
         image_category = request.GET.get("image")
         searched_images = Image.search_by_category(image_category)
         message = f"{image_category}"
 
-        return render(request, 'search.html',{"message":message,"images": searched_images})
+        return render(request, 'search.html',{"message":message,"images": searched_images, "categories":categories,'locations':locations})
 
     else:
         message = "You haven't searched for any term"
