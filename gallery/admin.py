d6bbb0f2474c76083111cf11de0e9469ff38a94a
faith62 <phifeichemu@gmail.com>
2022-06-03 10:33:47 +0300

    display list in image model

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Image,Category,Location
 
+class ImageAdmin(admin.ModelAdmin):    
+   
+    list_display = ('name', 'pic')
+
 # Register your models here.
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Category)
 admin.site.register(Location)
 
